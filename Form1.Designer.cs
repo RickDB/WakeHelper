aeb5504e66557a10240354c9cfdad7749c425c4d
Rick <RickDB@gmail.com>
2015-05-29 16:06:52 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WakeHelper
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.tbProgramName = new System.Windows.Forms.TextBox();
+      this.btnAddProgram = new System.Windows.Forms.Button();
+      this.lvProgramRestart = new System.Windows.Forms.ListView();
+      this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+      this.chEXEname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+      this.tbExeName = new System.Windows.Forms.TextBox();
+      this.lblProgramName = new System.Windows.Forms.Label();
+      this.lblExeName = new System.Windows.Forms.Label();
+      this.gbProgramsAfterWake = new System.Windows.Forms.GroupBox();
+      this.lblRestartDelay = new System.Windows.Forms.Label();
+      this.btnRemoveItem = new System.Windows.Forms.Button();
+      this.btnSave = new System.Windows.Forms.Button();
+      this.btnExitProgram = new System.Windows.Forms.Button();
+      this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+      this.tbRestartDelay = new System.Windows.Forms.TextBox();
+      this.gbProgramsAfterWake.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // tbProgramName
+      // 
+      this.tbProgramName.Location = new System.Drawing.Point(105, 36);
+      this.tbProgramName.Name = "tbProgramName";
+      this.tbProgramName.Size = new System.Drawing.Size(231, 20);
+      this.tbProgramName.TabIndex = 0;
+      // 
+      // btnAddProgram
+      // 
+      this.btnAddProgram.Location = new System.Drawing.Point(393, 39);
+      this.btnAddProgram.Name = "btnAddProgram";
+      this.btnAddProgram.Size = new System.Drawing.Size(75, 43);
+      this.btnAddProgram.TabIndex = 1;
+      this.btnAddProgram.Text = "Add";
+      this.btnAddProgram.UseVisualStyleBackColor = true;
+      this.btnAddProgram.Click += new System.EventHandler(this.btnAddProgram_Click);
+      // 
+      // lvProgramRestart
+      // 
+      this.lvProgramRestart.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chEXEname});
+      this.lvProgramRestart.Location = new System.Drawing.Point(0, 88);
+      this.lvProgramRestart.Name = "lvProgramRestart";
+      this.lvProgramRestart.Size = new System.Drawing.Size(468, 226);
+      this.lvProgramRestart.TabIndex = 2;
+      this.lvProgramRestart.UseCompatibleStateImageBehavior = false;
+      this.lvProgramRestart.View = System.Windows.Forms.View.Details;
+      // 
+      // chName
+      // 
+      this.chName.Text = "Name";
+      this.chName.Width = 247;
+      // 
+      // chEXEname
+      // 
+      this.chEXEname.Text = "Exe name";
+      this.chEXEname.Width = 151;
+      // 
+      // tbExeName
+      // 
+      this.tbExeName.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+      this.tbExeName.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+      this.tbExeName.Location = new System.Drawing.Point(105, 62);
+      this.tbExeName.Name = "tbExeName";
+      this.tbExeName.Size = new System.Drawing.Size(282, 20);
+      this.tbExeName.TabIndex = 3;
+      // 
+      // lblProgramName
+      // 
+      this.lblProgramName.AutoSize = true;
+      this.lblProgramName.Location = new System.Drawing.Point(5, 39);
+      this.lblProgramName.Name = "lblProgramName";
+      this.lblProgramName.Size = new System.Drawing.Size(75, 13);
+      this.lblProgramName.TabIndex = 4;
+      this.lblProgramName.Text = "Program name";
+      // 
+      // lblExeName
+      // 
+      this.lblExeName.AutoSize = true;
+      this.lblExeName.Location = new System.Drawing.Point(5, 65);
+      this.lblExeName.Name = "lblExeName";
+      this.lblExeName.Size = new System.Drawing.Size(65, 13);
+      this.lblExeName.TabIndex = 5;
+      this.lblExeName.Text = "Exe full path";
+      // 
+      // gbProgramsAfterWake
+      // 
+      this.gbProgramsAfterWake.Controls.Add(this.lblRestartDelay);
+      this.gbProgramsAfterWake.Controls.Add(this.tbRestartDelay);
+      this.gbProgramsAfterWake.Controls.Add(this.btnRemoveItem);
+      this.gbProgramsAfterWake.Controls.Add(this.lvProgramRestart);
+      this.gbProgramsAfterWake.Controls.Add(this.lblExeName);
+      this.gbProgramsAfterWake.Controls.Add(this.tbProgramName);
+      this.gbProgramsAfterWake.Controls.Add(this.lblProgramName);
+      this.gbProgramsAfterWake.Controls.Add(this.btnAddProgram);
+      this.gbProgramsAfterWake.Controls.Add(this.tbExeName);
+      this.gbProgramsAfterWake.Location = new System.Drawing.Point(12, 12);
+      this.gbProgramsAfterWake.Name = "gbProgramsAfterWake";
+      this.gbProgramsAfterWake.Size = new System.Drawing.Size(474, 349);
+      this.gbProgramsAfterWake.TabIndex = 6;
+      this.gbProgramsAfterWake.TabStop = false;
+      this.gbProgramsAfterWake.Text = "Programs to restart after wake";
+      // 
+      // lblRestartDelay
+      // 
+      this.lblRestartDelay.AutoSize = true;
+      this.lblRestartDelay.Location = new System.Drawing.Point(1, 326);
+      this.lblRestartDelay.Name = "lblRestartDelay";
+      this.lblRestartDelay.Size = new System.Drawing.Size(91, 13);
+      this.lblRestartDelay.TabIndex = 8;
+      this.lblRestartDelay.Text = "Restart delay (ms)";
+      // 
+      // btnRemoveItem
+      // 
+      this.btnRemoveItem.Location = new System.Drawing.Point(176, 323);
+      this.btnRemoveItem.Name = "btnRemoveItem";
+      this.btnRemoveItem.Size = new System.Drawing.Size(141, 23);
+      this.btnRemoveItem.TabIndex = 6;
+      this.btnRemoveItem.Text = "Remove from list";
+      this.btnRemoveItem.UseVisualStyleBackColor = true;
+      this.btnRemoveItem.Click += new System.EventHandler(this.btnRemoveItem_Click);
+      // 
+      // btnSave
+      // 
+      this.btnSave.Location = new System.Drawing.Point(12, 378);
+      this.btnSave.Name = "btnSave";
+      this.btnSave.Size = new System.Drawing.Size(139, 34);
+      this.btnSave.TabIndex = 7;
+      this.btnSave.Text = "Save and Close to tray";
+      this.btnSave.UseVisualStyleBackColor = true;
+      this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+      // 
+      // btnExitProgram
+      // 
+      this.btnExitProgram.Location = new System.Drawing.Point(350, 378);
+      this.btnExitProgram.Name = "btnExitProgram";
+      this.btnExitProgram.Size = new System.Drawing.Size(136, 34);
+      this.btnExitProgram.TabIndex = 7;
+      this.btnExitProgram.Text = "Exit program";
+      this.btnExitProgram.UseVisualStyleBackColor = true;
+      this.btnExitProgram.Click += new System.EventHandler(this.btnExitProgram_Click);
+      // 
+      // trayIcon
+      // 
+      this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+      this.trayIcon.Text = "Wake Helper";
+      this.trayIcon.Click += new System.EventHandler(this.trayIcon_Click);
+      // 
+      // tbRestartDelay
+      // 
+      this.tbRestartDelay.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::WakeHelper.Properties.Settings.Default, "restartDelay", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+      this.tbRestartDelay.Location = new System.Drawing.Point(98, 323);
+      this.tbRestartDelay.Name = "tbRestartDelay";
+      this.tbRestartDelay.Size = new System.Drawing.Size(63, 20);
+      this.tbRestartDelay.TabIndex = 7;
+      this.tbRestartDelay.Text = global::WakeHelper.Properties.Settings.Default.restartDelay;
+      this.tbRestartDelay.Validating += new System.ComponentModel.CancelEventHandler(this.tbRestartDelay_Validating);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(511, 428);
+      this.Controls.Add(this.btnExitProgram);
+      this.Controls.Add(this.btnSave);
+      this.Controls.Add(this.gbProgramsAfterWake);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Name = "Form1";
+      this.Text = "Wake Helper";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+      this.Resize += new System.EventHandler(this.Form1_Resize);
+      this.gbProgramsAfterWake.ResumeLayout(false);
+      this.gbProgramsAfterWake.PerformLayout();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox tbProgramName;
+    private System.Windows.Forms.Button btnAddProgram;
+    private System.Windows.Forms.ListView lvProgramRestart;
+    private System.Windows.Forms.ColumnHeader chName;
+    private System.Windows.Forms.ColumnHeader chEXEname;
+    private System.Windows.Forms.TextBox tbExeName;
+    private System.Windows.Forms.Label lblProgramName;
+    private System.Windows.Forms.Label lblExeName;
+    private System.Windows.Forms.GroupBox gbProgramsAfterWake;
+    private System.Windows.Forms.Button btnSave;
+    private System.Windows.Forms.Button btnExitProgram;
+    private System.Windows.Forms.NotifyIcon trayIcon;
+    private System.Windows.Forms.Button btnRemoveItem;
+    private System.Windows.Forms.Label lblRestartDelay;
+    private System.Windows.Forms.TextBox tbRestartDelay;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace WakeHelper
+{
+  public partial class Form1 : Form
+  {
+    public static List<string> listRestartProgram = new List<string>();
+    public Form1()
+    {
+      InitializeComponent();
+
+      loadSettings();
+
+      //Start sleep/wake monitor
+      monitorPowerState();
+
+      if (isProgramRunning("wakehelper", 0) > 1)
+      {
+        try
+        {
+          Environment.Exit(0);
+        }
+        catch
+        {
+        }
+      }
+
+      WindowState = FormWindowState.Minimized;
+    }
+    private static void monitorPowerState()
+    {
+      SystemEvents.PowerModeChanged += OnPowerModeChanged;
+    }
+
+    private static void OnPowerModeChanged(object sender, PowerModeChangedEventArgs e)
+    {
+      if (e.Mode == PowerModes.Resume)
+      {
+        try
+        {
+          foreach (var item in listRestartProgram)
+          {
+            string exeName = item.Split('\\').Last().ToLower().Replace(".exe", string.Empty);
+
+            Thread.Sleep(2500);
+
+            //MessageBox.Show("Killing process: " + exeName);
+            closeProcess(exeName);
+
+            Thread.Sleep(2500);
+
+            if (isProgramRunning(exeName, 0) <= 1)
+            {
+              // Restart process
+              int restartDelay = int.Parse(Properties.Settings.Default.restartDelay);
+              Thread.Sleep(restartDelay);
+              startProcess(item, "");
+            }
+            else
+            {
+              closeProcess(exeName);
+
+              if (isProgramRunning(exeName, 0) <= 1)
+              {
+                // Restart process
+                int restartDelay = int.Parse(Properties.Settings.Default.restartDelay);
+                Thread.Sleep(restartDelay);
+                startProcess(item, "");
+              }
+            }
+          }
+        }
+        catch (Exception)
+        {
+        }
+      }
+    }
+
+    public static void closeProcess(string exeName)
+    {
+      var restartProcess = Process.GetProcesses().
+        Where(
+          pr =>
+            pr.ProcessName.ToLower().Replace(".exe", string.Empty) == exeName);
+
+      foreach (var process in restartProcess)
+      {
+        //MessageBox.Show("Killed process!");
+        process.Kill();
+      }
+    }
+
+    public static int isProgramRunning(string name, int runtime)
+    {
+      foreach (Process clsProcess in Process.GetProcesses())
+      {
+        if (clsProcess.ProcessName.ToLower().Equals(name))
+        {
+          runtime++;
+        }
+      }
+      return runtime;
+    }
+
+    private static void startProcess(string program, string arguments)
+    {
+      try
+      {
+        //MessageBox.Show("Starting process: " + program);
+
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        startInfo.FileName = program;
+        Process proc = Process.Start(startInfo);
+      }
+      catch (Exception eStartProcess)
+      {
+      }
+
+      // Sleep timer to avoid Windows being to quick upon COM port unlocking
+      Thread.Sleep(2500);
+    }
+
+
+    private
+      void btnAddProgram_Click(object sender, EventArgs e)
+    {
+      ListViewItem lvItem = new ListViewItem();
+      lvItem.SubItems.Add(tbExeName.Text.Trim());
+      lvItem.Text = tbProgramName.Text.Trim();
+      lvProgramRestart.Items.Add(lvItem);
+
+      tbProgramName.Text = "";
+      tbExeName.Text = "";
+      tbProgramName.Focus();
+    }
+
+    private void btnSave_Click(object sender, EventArgs e)
+    {
+      trayIcon.Visible = true;
+      saveSettings();
+      this.Hide();
+    }
+
+    private void loadSettings()
+    {
+      try
+      {
+        string restartProcessList = Properties.Settings.Default.restartProcessList;
+        string[] processes = restartProcessList.Split(';');
+        listRestartProgram.Clear();
+
+        foreach (var itemprocess in processes)
+        {
+          string[] processesSplit = itemprocess.Split(',');
+
+          ListViewItem lvItem = new ListViewItem();
+          lvItem.SubItems.Add(processesSplit[1]);
+          lvItem.Text = processesSplit[0];
+          lvProgramRestart.Items.Add(lvItem);
+          listRestartProgram.Add(processesSplit[1]);
+        }
+      }
+      catch (Exception)
+      {
+      }
+    }
+    private void saveSettings()
+    {
+      listRestartProgram.Clear();
+      List<string> listItems = new List<string>();
+      string formattedSettingsList = "";
+
+      foreach (ListViewItem item in lvProgramRestart.Items)
+      {
+        string programName = item.Text;
+        string exeName = item.SubItems[1].Text;
+
+        listItems.Add(programName + "," + exeName);
+        listRestartProgram.Add(exeName);
+
+      }
+
+      foreach (var item in listItems)
+      {
+        if (string.IsNullOrEmpty(formattedSettingsList))
+        {
+          formattedSettingsList = item;
+        }
+        else
+        {
+          formattedSettingsList = formattedSettingsList + ";" + item;
+        }
+      }
+
+      //MessageBox.Show(formattedSettingsList);
+
+      Properties.Settings.Default.restartProcessList = formattedSettingsList;
+      Properties.Settings.Default.Save();
+    }
+
+    private void btnExitProgram_Click(object sender, EventArgs e)
+    {
+      saveSettings();
+
+      if (trayIcon.Visible)
+      {
+        try
+        {
+          trayIcon.Visible = false;
+          trayIcon.Dispose();
+        }
+        catch (Exception)
+        {
+        }
+      }
+
+      Environment.Exit(0);
+    }
+
+    private void trayIcon_Click(object sender, EventArgs e)
+    {
+      this.WindowState = FormWindowState.Normal;
+      this.Show();
+    }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      saveSettings();
+      e.Cancel = true;
+      trayIcon.Visible = true;
+      this.Hide();
+    }
+
+    private void btnRemoveItem_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        List<ListViewItem> currentItems = new List<ListViewItem>();
+        String selectedItem = lvProgramRestart.SelectedItems[0].Text;
+        foreach (ListViewItem item in lvProgramRestart.Items)
+        {
+          if (item.Text != selectedItem)
+          {
+            currentItems.Add(item);
+          }
+        }
+
+        lvProgramRestart.Items.Clear();
+
+        foreach (ListViewItem item in currentItems)
+        {
+          lvProgramRestart.Items.Add(item);
+        }
+      }
+      catch (Exception et)
+      {
+        MessageBox.Show("Error while removing item " + et.Message);
+      }
+    }
+
+    private void tbRestartDelay_Validating(object sender, CancelEventArgs e)
+    {
+      Properties.Settings.Default.Save();
+    }
+
+    private void Form1_Resize(object sender, EventArgs e)
+    {
+      trayIcon.Visible = true;
+      this.Hide();
+    }
+  }
+}
